acdb81b6c36ab2613e2975695077ba00d448554b
Michael Waskom <mwaskom@stanford.edu>
2013-10-28 19:45:42 -0700

    Adding axlabel to example plot

--- examples/example_plot.py
+++ examples/example_plot.py
@@ -6,5 +6,6 @@ import seaborn as sns
 x = np.linspace(0, 5, 100)
 for n in range(1, 6):
     plt.plot(x, jn(x, n))
+sns.axlabel("The abscissa", "The ordinate")
 plt.title("Default Seaborn Aesthetics")
 plt.savefig("examples/example_plot.png")
