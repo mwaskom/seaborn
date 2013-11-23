5fb0c1477441fbf46afd3d7d8e3564ffc5d36674
chadheyne <chadheyne@smeal.psu.edu>
2013-11-23 02:01:26 -0500

    Fix unhashable type: list

--- seaborn/utils.py
+++ seaborn/utils.py
@@ -52,7 +52,7 @@ def color_palette(name=None, n_colors=6, desat=None):
 
     if name is None:
         palette = mpl.rcParams["axes.color_cycle"]
-    elif name in seaborn_palettes:
+    elif isinstance(name, str) and name in seaborn_palettes:
         palette = seaborn_palettes[name]
     elif hasattr(name, "__iter__") and not isinstance(name, str):
         palette = name
@@ -60,8 +60,6 @@ def color_palette(name=None, n_colors=6, desat=None):
         palette = hls_palette(n_colors)
     elif name == "husl":
         palette = husl_palette(n_colors)
-    elif name in seaborn_palettes:
-        palette = seaborn_palettes[name]
     elif name in dir(mpl.cm):
         palette = mpl_palette(name, n_colors)
     else:
