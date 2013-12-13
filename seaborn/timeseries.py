e7ae0f0251cdc6362047c669d28aed018bfc8985
mwaskom <mwaskom@stanford.edu>
2013-12-12 22:14:48 -0800

    Fix indentation issue in tsplot docstring

--- seaborn/timeseries.py
+++ seaborn/timeseries.py
@@ -50,8 +50,8 @@ def tsplot(data, time=None, unit=None, condition=None, value=None,
         legend (unless legend is set to False).
     err_style : string or list of strings or None
         Names of ways to plot uncertainty across units from set of
-       {ci_band, ci_bars, boot_traces, book_kde, unit_traces, unit_points}.
-       Can use one or more than one method.
+        {ci_band, ci_bars, boot_traces, book_kde, unit_traces, unit_points}.
+        Can use one or more than one method.
     ci : float or list of floats in [0, 100]
         Confidence interaval size(s). If a list, it will stack the error
         plots for each confidence interval. Only relevant for error styles
