427704c9b16d760867284f729d760d114aaad5b2
mwaskom <mwaskom@stanford.edu>
2013-12-12 16:44:11 -0800

    Edit __init__ file with new modules

--- seaborn/__init__.py
+++ seaborn/__init__.py
@@ -1,12 +1,9 @@
-from .rcmod import *
-from .utils import *
-from .palettes import *
-from .linearmodels import *
-from .distributions import *
-from .timeseries import *
-from .miscplot import *
-from .axisgrid import *
-from .xkcd_rgb import xkcd_rgb
+from rcmod import *
+from utils import *
+from linearmodels import *
+from distributions import *
+from timeseries import *
+from miscplot import *
 set()
 
 __version__ = "0.2.dev"
