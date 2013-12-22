c8f6806c506bb6771128f581d1c5a8be57c35bdf
mwaskom <mwaskom@stanford.edu>
2013-12-21 23:15:41 -0800

    Py3k compatability changes

--- seaborn/__init__.py
+++ seaborn/__init__.py
@@ -1,9 +1,9 @@
-from rcmod import *
-from utils import *
-from linearmodels import *
-from distributions import *
-from timeseries import *
-from miscplot import *
+from .rcmod import *
+from .utils import *
+from .linearmodels import *
+from .distributions import *
+from .timeseries import *
+from .miscplot import *
 set()
 
 __version__ = "0.2.dev"
