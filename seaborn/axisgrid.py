db9c8b1aa70d454a87a7f91d0328991dc9e29e42
mwaskom <mwaskom@stanford.edu>
2014-01-06 21:54:13 -0800

    Add tests and docstrings to FacetGrid

--- seaborn/axisgrid.py
+++ seaborn/axisgrid.py
@@ -11,6 +11,7 @@ class FacetGrid(object):
 
     def __init__(self, data, row=None, col=None, hue=None, col_wrap=None,
                  sharex=True, sharey=True, size=3, aspect=1, palette="husl",
+                 row_order=None, col_order=None, hue_order=None,
                  dropna=True, legend=True, legend_out=True, despine=True,
                  margin_titles=False, xlim=None, ylim=None):
         """Initialize the plot figure and FacetGrid object.
@@ -31,8 +32,12 @@ class FacetGrid(object):
             Height (in inches) of each facet.
         aspect : scalar, optional
             Aspect * size gives the width (in inches) of each facet.
-        palette : seaborn color palette
-            Set of colors for mapping the `hue` variable.
+        palette : dict or seaborn color palette
+            Set of colors for mapping the `hue` variable. If a dict, keys
+            should be values  in the `hue` variable.
+        {row, col, hue}_order: sequence of strings
+            Order to plot the values in the faceting variables in, otherwise
+            sorts the unique values.
         dropna : boolean, optional
             Drop missing values from the data before plotting.
         legend : boolean, optional
@@ -44,7 +49,7 @@ class FacetGrid(object):
         margin_titles : boolean, optional
             Write the column and row variable labels on the margins of the
             grid rather than above each plot.
-        {x, y}lim: two-tuples, optional
+        {x, y}lim: tuples, optional
             Limits for each of the axes on each facet when share{x, y} is True.
 
         Returns
@@ -60,6 +65,11 @@ class FacetGrid(object):
         # This can get stretched later by a legend
         figsize = (ncol * size * aspect, nrow * size)
 
+        # Validate some inputs
+        if col_wrap is not None:
+            margin_titles = False
+
+        # Build the subplot keyword dictionary
         subplot_kw = {}
         if xlim is not None:
             subplot_kw["xlim"] = xlim
@@ -71,55 +81,80 @@ class FacetGrid(object):
                                  sharex=sharex, sharey=sharey,
                                  subplot_kw=subplot_kw)
 
+        # Determine the hue facet layer information
         hue_var = hue
         if hue is None:
             hue_names = None
             hue_masks = [np.repeat(True, len(data))]
-            colors = None
+            # Use the first color of the current palette
+            # I'm not sure if I like this vs. e.g. dark gray
+            colors = utils.color_palette(n_colors=1)
         else:
-            hue_names = np.sort(data[hue].unique())
+            if hue_order is None:
+                hue_names = np.sort(data[hue].unique())
+            else:
+                hue_names = hue_order
             if dropna:
+                # Filter NA from the list of unique hue names
                 hue_names = list(filter(pd.notnull, hue_names))
             if isinstance(palette, dict):
+                # Allow for palette to map from hue variable names
                 palette = [palette[h] for h in hue_names]
             hue_masks = [data[hue] == val for val in hue_names]
             colors = utils.color_palette(palette, len(hue_masks))
 
-        all_na = np.zeros(len(data), np.bool)
+        # Make a boolean mask that is True anywhere there is an NA
+        # value in one of the faceting variables, but only if dropna is True
+        none_na = np.zeros(len(data), np.bool)
         if dropna:
-            row_na = all_na if row is None else data[row].isnull()
-            col_na = all_na if col is None else data[col].isnull()
-            hue_na = all_na if hue is None else data[hue].isnull()
+            row_na = none_na if row is None else data[row].isnull()
+            col_na = none_na if col is None else data[col].isnull()
+            hue_na = none_na if hue is None else data[hue].isnull()
             not_na = ~(row_na & col_na & hue_na)
         else:
-            not_na = ~all_na
+            not_na = ~none_na
 
+        # Set up the lists of names for the row and column facet variables
+        if row is None:
+            row_names = []
+        elif row_order is None:
+            row_names = sorted(data[row].unique())
+        else:
+            row_names = row_order
+        if dropna:
+            row_names = list(filter(pd.notnull, row_names))
+
+        if col is None:
+            col_names = []
+        elif col_order is None:
+            col_names = sorted(data[col].unique())
+        else:
+            col_names = col_order
+        if dropna:
+            col_names = list(filter(pd.notnull, col_names))
+
+        # Set up the class attributes
+        # ---------------------------
+
+        # First the public API
+        # TODO decide what we want to go here...
         self._data = data
         self._fig = fig
         self._axes = axes
+
+        self._row_names = row_names
+        self._col_names = col_names
+        self._hue_names = hue_names
+
+        # Next the private variables
         self._nrow = nrow
         self._row_var = row
-        if row is None:
-            self._row_names = []
-        else:
-            row_names = sorted(data[row].unique())
-            if dropna:
-                row_names = list(filter(pd.notnull, row_names))
-            self._row_names = row_names
-
         self._ncol = ncol
         self._col_var = col
-        if col is None:
-            self._col_names = []
-        else:
-            col_names = sorted(data[col].unique())
-            if dropna:
-                col_names = list(filter(pd.notnull, col_names))
-            self._col_names = col_names
+
         self._margin_titles = margin_titles
         self._col_wrap = col_wrap
         self._hue_var = hue_var
-        self._hue_names = hue_names
         self._colors = colors
         self._draw_legend = ((hue is not None and hue not in [col, row])
                              and legend)
@@ -131,29 +166,45 @@ class FacetGrid(object):
         self._dropna = dropna
         self._not_na = not_na
 
+        # Make the axes look good
         fig.tight_layout()
         if despine:
             utils.despine(self._fig)
 
     def facet_data(self):
+        """Generator for name indices and data subsets for each facet.
 
+        Yields
+        ------
+        (i, j, k), data_ijk : tuple of ints, DataFrame
+            The ints provide an index into the {row, col, hue}_names attribute,
+            and the dataframe contains a subset of the full data corresponding
+            to each facet. The generator yields subsets that correspond with
+            the self.axes.flat iterator, or self.axes[i, j] when `col_wrap`
+            is None.
+
+        """
         data = self._data
 
+        # Construct masks for the row variable
         if self._nrow == 1 or self._col_wrap is not None:
             row_masks = [np.repeat(True, len(self._data))]
         else:
             row_masks = [data[self._row_var] == n for n in self._row_names]
 
+        # Construct masks for the column variable
         if self._ncol == 1:
             col_masks = [np.repeat(True, len(self._data))]
         else:
             col_masks = [data[self._col_var] == n for n in self._col_names]
 
+        # Construct masks for the hue variable
         if len(self._colors) == 1:
             hue_masks = [np.repeat(True, len(self._data))]
         else:
             hue_masks = [data[self._hue_var] == n for n in self._hue_names]
 
+        # Here is the main generator loop
         for (i, row), (j, col), (k, hue) in product(enumerate(row_masks),
                                                     enumerate(col_masks),
                                                     enumerate(hue_masks)):
@@ -161,142 +212,240 @@ class FacetGrid(object):
             yield (i, j, k), data_ijk
 
     def map(self, func, *args, **kwargs):
+        """Apply a plotting function to each facet's subset of the data.
 
-        for (row_i, col_j, hue_k), data_ijk in self._iter_masks():
+        Parameters
+        ----------
+        func : callable
+            A plotting function that takes data and keyword arguments. It
+            must plot to the currently active matplotlib Axes and take a
+            `color` keyword argument. If faceting on the `hue` dimension,
+            it must also take a `label` keyword argument.
+        args : strings
+            Column names in self.data that identify variables with data to
+            plot. The data for each variable is passed to `func` in the
+            order the variables are specified in the call.
+        kwargs : keyword arguments
+            All keyword arguments are passed to the plotting function.
 
+        Returns
+        -------
+        self : object
+            Returns self.
+
+        """
+
+        # If color was a keyword argument, grab it here
+        kw_color = kwargs.pop("color", None)
+
+        # Iterate over the data subsets
+        for (row_i, col_j, hue_k), data_ijk in self.facet_data():
+
+            # If this subset is null, move on
+            if not data_ijk.values.tolist():
+                continue
+
+            # Calculate the actual indices of the axes to plot on
             if self._col_wrap is not None:
                 f_row = col_j // self._ncol
                 f_col = col_j % self._ncol
             else:
                 f_row, f_col = row_i, col_j
 
+            # Get a reference to the axes object we want, and make it active
             ax = self._axes[f_row, f_col]
             plt.sca(ax)
 
-            if not data_ijk.values.tolist():
-                continue
-
+            # Decide what color to plot with
             color = self._colors[hue_k]
-            if color is not None:
-                kwargs["color"]
+            if kw_color is not None:
+                kwargs["color"] = kw_color
+            elif color is not None:
+                kwargs["color"] = color
 
+            # Insert a label in the keyword arguments for the legend
             if self._hue_var is not None:
                 kwargs["label"] = self._hue_names[hue_k]
 
+            # Get the actual data we are going to plot with
             plot_data = data_ijk[list(args)]
             if self._dropna:
                 plot_data = plot_data.dropna()
-            plot_args = [v.values for k, v in plot_data.iteritems()]
+            plot_args = [v for k, v in plot_data.iteritems()]
+
+            # Draw the plot
             func(*plot_args, **kwargs)
 
+            # Sort out the supporting information
             self._update_legend_data(ax)
             self._clean_axis(ax)
 
-        self._set_axis_labels(*args[:2])
-
+        # Finalize the annotations and layout
+        self.set_axis_labels(*args[:2])
         if self._draw_legend:
             self._make_legend()
         else:
             self._fig.tight_layout()
-        self._set_title()
+        self.set_titles()
+
+        return self
 
     def set(self, **kwargs):
+        """Set axis attributes on each facet.
 
+        This will call set_{key}({value}) for each keyword argument on every
+        facet in the grid.
+
+        """
         for key, val in kwargs.items():
             for ax in self._axes.flat:
                 setter = getattr(ax, "set_%s" % key)
                 setter(val)
 
-    def despine(self, **kwargs):
+        return self
 
+    def despine(self, **kwargs):
+        """Remove axis spines from the facets."""
         utils.despine(self._fig, **kwargs)
+        return self
 
     def set_axis_labels(self, x_var, y_var=None):
-
+        """Set axis labels on the left column and bottom row of the grid."""
         if y_var is not None:
             self._y_var = y_var
-            self.set_ylabel(y_var)
-        self.set_xlabel(x_var)
+            self.set_ylabels(y_var)
         self._x_var = x_var
+        self.set_xlabels(x_var)
+        return self
 
     def set_xlabels(self, label=None, **kwargs):
-
+        """Label the x axis on the bottom row of the grid."""
         if label is None:
             label = self._x_var
         for ax in self._axes[-1, :]:
             ax.set_xlabel(label, **kwargs)
+        return self
 
     def set_ylabels(self, label=None, **kwargs):
-
+        """Label the y axis on the left column of the grid."""
         if label is None:
             label = self._y_var
         for ax in self._axes[:, 0]:
             ax.set_ylabel(label, **kwargs)
+        return self
 
     def set_xticklabels(self, labels=None, **kwargs):
-
+        """Set x axis tick labels on the bottom row of the grid."""
         for ax in self._axes[-1, :]:
             if labels is None:
                 labels = [l.get_text() for l in ax.get_xticklabels()]
             ax.set_xticklabels(labels, **kwargs)
+        return self
 
     def set_yticklabels(self, labels=None, **kwargs):
-
+        """Set y axis tick labels on the left column of the grid."""
         for ax in self._axes[-1, :]:
             if labels is None:
                 labels = [l.get_text() for l in ax.get_yticklabels()]
             ax.set_yticklabels(labels, **kwargs)
+        return self
 
-    def _clean_axis(self, ax):
+    def set_titles(self, template=None, row_template=None,  col_template=None,
+                   **kwargs):
+        """Draw titles either above each facet or on the grid margins.
 
-        ax.set_xlabel("")
-        ax.set_ylabel("")
-        ax.legend_ = None
+        Parameters
+        ----------
+        template : string
+            Template for all titles with the formatting keys {col_var} and
+            {col_name} (if using a `col` faceting variable) and/or {row_var}
+            and {row_name} (if using a `row` faceting variable).
+        row_template: 
+            Template for the row variable when titles are drawn on the grid
+            margins. Must have {row_var} and {row_name} formatting keys.
+        col_template: 
+            Template for the row variable when titles are drawn on the grid
+            margins. Must have {col_var} and {col_name} formatting keys.
 
-    def _set_title(self):
+        Returns
+        -------
+        self: object
+            Returns self.
+
+        """
+        args = dict(row_var=self._row_var, col_var=self._col_var)
+        kwargs["size"] = kwargs.pop("size", mpl.rcParams["axes.labelsize"])
+
+        # Establish default templates
+        if row_template is None:
+            row_template = "{row_var} = {row_name}"
+        if col_template is None:
+            col_template = "{col_var} = {col_name}"
+        if template is None:
+            if self._row_var is None:
+                template = col_template
+            elif self._col_var is None:
+                template = row_template
+            else:
+                template = " | ".join([row_template, col_template])
 
         if self._margin_titles:
             if self._row_names:
+                # Draw the row titles on the right edge of the grid
                 for i, row_name in enumerate(self._row_names):
                     ax = self._axes[i, -1]
-                    title = "%s = %s" % (self._row_var, row_name)
+                    args.update(dict(row_name=row_name))
+                    title = row_template.format(**args)
                     trans = self._fig.transFigure.inverted()
                     bbox = ax.bbox.transformed(trans)
                     x = bbox.xmax + 0.01
                     y = bbox.ymax - (bbox.height / 2)
-                    size = mpl.rcParams["axes.titlesize"]
-                    self._fig.text(x, y, title, rotation=270, size=size,
-                                   ha="left", va="center")
+                    self._fig.text(x, y, title, rotation=270,
+                                   ha="left", va="center", **kwargs)
             if self._col_names:
+                # Draw the column titles  as normal titles
                 for j, col_name in enumerate(self._col_names):
-                    title = "%s = %s" % (self._col_var, col_name)
-                    self._axes[0, j].set_title(title)
+                    args.update(dict(col_name=col_name))
+                    title = col_template.format(**args)
+                    self._axes[0, j].set_title(title, **kwargs)
 
-            return
+            return self
 
-        if self._row_names and self._col_names:
+        # Otherwise title each facet with all the necessary information
+        if (self._row_var is not None) and (self._col_var is not None):
             for i, row_name in enumerate(self._row_names):
                 for j, col_name in enumerate(self._col_names):
-                    title = "%s = %s | %s = %s" % (self._row_var, row_name,
-                                                   self._col_var, col_name)
-                    self._axes[i, j].set_title(title)
+                    args.update(dict(row_name=row_name, col_name=col_name))
+                    title = template.format(**args)
+                    self._axes[i, j].set_title(title, **kwargs)
         elif self._row_names:
             for i, row_name in enumerate(self._row_names):
-                title = "%s = %s" % (self._row_var, row_name)
-                self._axes[i, 0].set_title(title)
+                args.update(dict(row_name=row_name))
+                title = template.format(**args)
+                self._axes[i, 0].set_title(title, **kwargs)
         elif self._col_names:
             for i, col_name in enumerate(self._col_names):
-                title = "%s = %s" % (self._col_var, col_name)
-                self._axes[0, i].set_title(title)
+                args.update(dict(col_name=col_name))
+                title = template.format(**args)
+                # Index the flat array so col_wrap works
+                self._axes.flat[i].set_title(title, **kwargs)
+        return self
+
+    def _clean_axis(self, ax):
+        """Turn off axis labels and legend."""
+        ax.set_xlabel("")
+        ax.set_ylabel("")
+        ax.legend_ = None
+        return self
 
     def _update_legend_data(self, ax):
-
+        """Extract the legend data from an axes object and save it."""
         handles, labels = ax.get_legend_handles_labels()
         data = {l: h for h, l in zip(handles, labels)}
         self._legend_data.update(data)
 
     def _make_legend(self, legend_data=None, title=None):
-
+        """Draw a legend, possibly resizing the figure."""
         legend_data = self._legend_data if legend_data is None else legend_data
         labels = sorted(self._legend_data.keys())
         handles = [legend_data[l] for l in labels]
