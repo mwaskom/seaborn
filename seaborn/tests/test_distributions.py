a4f9bfc837af811c6181e90521626b8a437dd6b0
mwaskom <mwaskom@stanford.edu>
2014-01-13 23:35:01 -0800

    Improve kde computation

--- seaborn/tests/test_distributions.py
+++ seaborn/tests/test_distributions.py
@@ -161,3 +161,55 @@ class TestBoxReshaping(object):
         with nt.assert_raises(ValueError):
             dist._box_reshape(self.x, None, None, range(5))
 
+
+class TestKDE(object):
+
+    rs = np.random.RandomState(0)
+    x = rs.randn(50)
+    y = rs.randn(50)
+    kernel = "gau"
+    bw = "scott"
+    gridsize = 128
+    clip = (-np.inf, np.inf)
+    cut = 3
+
+    def test_scipy_univariate_kde(self):
+        """Test the univariate KDE estimation with scipy."""
+        grid, y = dist._scipy_univariate_kde(self.x, self.bw, self.gridsize,
+                                             self.cut, self.clip)
+        nt.assert_equal(len(grid), self.gridsize)
+        nt.assert_equal(len(y), self.gridsize)
+        for bw in ["silverman", .2]:
+            dist._scipy_univariate_kde(self.x, bw, self.gridsize,
+                                       self.cut, self.clip)
+
+    def test_statsmodels_univariate_kde(self):
+        """Test the univariate KDE estimation with statsmodels."""
+        grid, y = dist._statsmodels_univariate_kde(self.x, self.kernel,
+                                                   self.bw, self.gridsize,
+                                                   self.cut, self.clip)
+        nt.assert_equal(len(grid), self.gridsize)
+        nt.assert_equal(len(y), self.gridsize)
+        for bw in ["silverman", .2]:
+            dist._statsmodels_univariate_kde(self.x, self.kernel, bw,
+                                             self.gridsize, self.cut,
+                                             self.clip)
+
+    def test_scipy_bivariate_kde(self):
+        """Test the bivariate KDE estimation with scipy."""
+        clip = [self.clip, self.clip]
+        x, y, z = dist._scipy_bivariate_kde(self.x, self.y, self.bw,
+                                            self.gridsize, self.cut, clip)
+        nt.assert_equal(x.shape, (self.gridsize, self.gridsize))
+        nt.assert_equal(y.shape, (self.gridsize, self.gridsize))
+        nt.assert_equal(len(z), self.gridsize)
+
+    def test_statsmodels_bivariate_kde(self):
+        """Test the bivariate KDE estimation with statsmodels."""
+        clip = [self.clip, self.clip]
+        x, y, z = dist._statsmodels_bivariate_kde(self.x, self.y, self.bw,
+                                                  self.gridsize,
+                                                  self.cut, clip)
+        nt.assert_equal(x.shape, (self.gridsize, self.gridsize))
+        nt.assert_equal(y.shape, (self.gridsize, self.gridsize))
+        nt.assert_equal(len(z), self.gridsize)
