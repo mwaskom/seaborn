72e26e91487dfb9c389ffd001264666e707c7e33
mwaskom <mwaskom@stanford.edu>
2014-03-08 13:44:39 -0800

    Initial implementation of MarginGrid

--- seaborn/axisgrid.py
+++ seaborn/axisgrid.py
@@ -583,3 +583,231 @@ class FacetGrid(object):
         handles, labels = ax.get_legend_handles_labels()
         data = {l: h for h, l in zip(handles, labels)}
         self._legend_data.update(data)
+
+
+class MarginGrid(object):
+    """Grid for drawing a bivariate plot with marginal univariate plots."""
+    def __init__(self, x, y, data=None, size=6, ratio=5, dropna=True,
+                 xlim=None, ylim=None):
+        """Set up the grid of subplots.
+
+        Parameters
+        ----------
+        x, y : strings or vectors
+            Data or names of variables in `data`.
+        data : DataFrame, optional
+            DataFrame when `x` and `y` are variable names.
+        size : numeric
+            Size of the figure (it will be square).
+        ratio : numeric
+            Ratio of joint axes size to marginal axes height.
+        dropna : bool, optional
+            If True, remove observations that are missing from `x` and `y`.
+        {x, y}lim : two-tuples, optional
+            Axis limits to set before plotting.
+
+        """
+        # Set up the subplot grid
+        f = plt.figure(figsize=(size, size))
+        gs = plt.GridSpec(ratio + 1, ratio + 1)
+
+        ax_joint = f.add_subplot(gs[1:, :-1])
+        ax_marg_x = f.add_subplot(gs[0, :-1], sharex=ax_joint)
+        ax_marg_y = f.add_subplot(gs[1:, -1], sharey=ax_joint)
+
+        self.fig = f
+        self.ax_joint = ax_joint
+        self.ax_marg_x = ax_marg_x
+        self.ax_marg_y = ax_marg_y
+
+        # Turn off tick visibility for the measure axis on the marginal plots
+        plt.setp(ax_marg_x.get_xticklabels(), visible=False)
+        plt.setp(ax_marg_y.get_yticklabels(), visible=False)
+
+        # Turn off the ticks on the density axis for the marginal plots
+        ax_marg_x.set_yticks([])
+        ax_marg_y.set_xticks([])
+
+        # Possibly extract the variables from a DataFrame
+        if data is not None:
+            if x in data:
+                x = data[x]
+            if y in data:
+                y = data[y]
+
+        # Possibly drop NA
+        if dropna:
+            not_na = pd.notnull(x) & pd.notnull(y)
+            x = x[not_na]
+            y = y[not_na]
+
+        # Find the names of the variables
+        if hasattr(x, "name"):
+            xlabel = x.name
+            ax_joint.set_xlabel(xlabel)
+        if hasattr(y, "name"):
+            ylabel = y.name
+            ax_joint.set_ylabel(ylabel)
+
+        # Convert the x and y data to arrays for plotting
+        self.x = np.asarray(x)
+        self.y = np.asarray(y)
+
+        if xlim is not None:
+            ax_joint.set_xlim(xlim)
+        if ylim is not None:
+            ax_joint.set_ylim(ylim)
+
+        # Make the grid look nice
+        utils.despine(f)
+        utils.despine(ax=ax_marg_x, left=True)
+        utils.despine(ax=ax_marg_y, bottom=True)
+        f.tight_layout()
+
+    def plot(self, joint_func, marginal_func):
+        """Shortcut to draw the full plot.
+
+        Use `plot_joint` and `plot_marginals` directly for more control.
+
+        Parameters
+        ----------
+        joint_func, marginal_func: callables
+            Functions to draw the bivariate and univariate plots.
+
+        Returns
+        -------
+        self : MarginGrid instance
+            Returns `self`.
+
+        """
+        self.plot_marginals(marginal_func)
+        self.plot_joint(joint_func)
+        return self
+
+    def plot_joint(self, func, **kwargs):
+        """Draw a bivariate plot of `x` and `y`.
+
+        Parameters
+        ----------
+        func : plotting callable
+            This must take two 1d arrays of data as the first two
+            positional arguments, and it must plot on the "current" axes.
+        kwargs : key, value mappings
+            Keyword argument are passed to the plotting function.
+
+        Returns
+        -------
+        self : MarginGrid instance
+            Returns `self`.
+
+        """
+        plt.sca(self.ax_joint)
+        func(self.x, self.y, **kwargs)
+
+    def plot_marginals(self, func, **kwargs):
+        """Draw univariate plots for `x` and `y` separately.
+
+        Parameters
+        ----------
+        func : plotting callable
+            This must take a 1d array of data as the first positional
+            argument, it must plot on the "current" axes, and it must
+            accept a "vertical" keyword argument to orient the measure
+            dimension of the plot vertically.
+        kwargs : key, value mappings
+            Keyword argument are passed to the plotting function.
+
+        Returns
+        -------
+        self : MarginGrid instance
+            Returns `self`.
+
+        """
+        plt.sca(self.ax_marg_x)
+        func(self.x, **kwargs)
+
+        kwargs["vertical"] = True
+        plt.sca(self.ax_marg_y)
+        func(self.y, **kwargs)
+
+        return self
+
+    def annotate(self, func, template=None, stat=None, loc="best", **kwargs):
+        """Annotate the plot with a statistic about the relationship.
+
+        Parameters
+        ----------
+        func : callable
+            Statistical function that maps the x, y vectors either to (val, p)
+            or to val.
+        template : string format template, optional
+            The template must have the format keys "stat" and "val";
+            if `func` returns a p value, it should also have the key "p".
+        stat : string, optional
+            Name to use for the statistic in the annotation, by default it
+            uses the name of `func`.
+        loc : string or int, optional
+            Matplotlib legend location code; used to place the annotation.
+        kwargs : key, value mappings
+            Other keyword arguments are passed to `ax.legend`, which formats
+            the annotation.
+
+        Returns
+        -------
+        self : MarginGrid instance.
+            Returns `self`.
+
+        """
+        default_template = "{stat} = {val:.2g}; p = {p:.2g}"
+
+        # Call the function and determine the form of the return value(s)
+        out = func(self.x, self.y)
+        try:
+            val, p = out
+        except TypeError:
+            val, p = out, None
+            default_template, _ = default_template.split(";")
+
+        # Set the default template
+        if template is None:
+            template = default_template
+
+        # Default to name of the function
+        if stat is None:
+            stat = func.__name__
+
+        # Format the annotation
+        if p is None:
+            annotation = template.format(stat=stat, val=val)
+        else:
+            annotation = template.format(stat=stat, val=val, p=p)
+
+        # Draw an invisible plot and use the legend to draw the annotation
+        # This is a bit of a hack, but `loc=best` works nicely and is not
+        # easily abstracted.
+        phantom, = self.ax_joint.plot(self.x, self.y, linestyle="", alpha=0)
+        self.ax_joint.legend([phantom], [annotation], loc=loc, **kwargs)
+        phantom.remove()
+
+        return self
+
+    def set_axis_labels(self, xlabel="", ylabel="", **kwargs):
+        """Set the axis labels on the bivariate axes.
+
+        Parameters
+        ----------
+        xlabel, ylabel : strings
+            Label names for the x and y variables.
+        kwargs : key, value mappings
+            Other keyword arguments are passed to the set_xlabel or
+            set_ylabel.
+
+        Returns
+        -------
+        self : MarginGrid instance
+            returns `self`
+
+        """
+        self.ax_joint.set_xlabel(xlabel, **kwargs)
+        self.ax_joint.set_ylabel(ylabel, **kwargs)
+        return self
