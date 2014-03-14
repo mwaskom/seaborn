b99667730419914b1a088559568f14c0bda23933
mwaskom <mwaskom@stanford.edu>
2014-03-14 10:34:09 -0700

    Add initial example scripts

--- examples/distplot_options.py
+++ examples/distplot_options.py
@@ -7,7 +7,7 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-sns.set(style="white", palette="muted")
+sns.set(style="nogrid", palette="muted")
 f, axes = plt.subplots(2, 2, figsize=(7, 7), sharex=True)
 sns.despine(left=True)
 
@@ -17,10 +17,10 @@ b, g, r, p = sns.color_palette("muted", 4)
 
 d = rs.normal(size=100)
 
-sns.distplot(d, kde=False, color=b, ax=axes[0, 0])
+sns.distplot(d, color=b, ax=axes[0, 0])
 sns.distplot(d, hist=False, rug=True, color=r, ax=axes[0, 1])
 sns.distplot(d, hist=False, color=g, kde_kws={"shade": True}, ax=axes[1, 0])
-sns.distplot(d, color=p, ax=axes[1, 1])
+sns.distplot(d, kde=False, color=p, ax=axes[1, 1])
 
 plt.setp(axes, yticks=[])
 plt.tight_layout()
