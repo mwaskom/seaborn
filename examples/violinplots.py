07b489769f5f2e35778258f5a61d3652eb3de7ca
mwaskom <mwaskom@stanford.edu>
2014-03-14 10:34:09 -0700

    Add initial example scripts

--- examples/violinplots.py
+++ examples/violinplots.py
@@ -5,7 +5,6 @@ Violinplots
 """
 import numpy as np
 import seaborn as sns
-import matplotlib.pyplot as plt
 
 sns.set(style="whitegrid")
 
@@ -15,6 +14,5 @@ n, p = 40, 8
 d = rs.normal(0, 1, (n, p))
 d += np.log(np.arange(1, p + 1)) * -5 + 10
 
-f, ax = plt.subplots()
 sns.violinplot(d)
 sns.despine(left=True)
