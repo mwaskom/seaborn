3c101306a706fd1dadb3cab93cf4dd2319df051e
mwaskom <mwaskom@stanford.edu>
2014-03-14 10:34:09 -0700

    Add initial example scripts

--- examples/grouped_boxplot.py
+++ examples/grouped_boxplot.py
@@ -4,12 +4,12 @@ Grouped boxplots
 
 """
 import seaborn as sns
-sns.set(style="ticks")
+sns.set(style="whitegrid")
 
 tips = sns.load_dataset("tips")
 days = ["Thur", "Fri", "Sat", "Sun"]
 
 g = sns.factorplot("day", "total_bill", "sex", tips, kind="box",
                    palette="PRGn", aspect=1.25, x_order=days)
-g.despine(offset=10, trim=True)
+g.despine(left=True)
 g.set_axis_labels("Day", "Total Bill")
