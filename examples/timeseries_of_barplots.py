a305b7041752dbca928e52b19b62647dce5dfcd2
mwaskom <mwaskom@stanford.edu>
2014-03-15 16:42:52 -0700

    Add and tweak many example scripts

--- examples/timeseries_of_barplots.py
+++ examples/timeseries_of_barplots.py
@@ -9,4 +9,6 @@ import seaborn as sns
 sns.set(style="nogrid")
 planets = sns.load_dataset("planets")
 years = np.arange(2000, 2015)
-sns.factorplot("year", data=planets, palette="BuPu", aspect=1.5, x_order=years)
+g = sns.factorplot("year", data=planets, palette="BuPu",
+                   aspect=1.5, x_order=years)
+g.set_xticklabels(step=2)
