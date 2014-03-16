95a91cca58303a6fab167d51d79a8c6a5ebd04ff
mwaskom <mwaskom@stanford.edu>
2014-03-15 18:14:59 -0700

    Let example scripts specify their thumbnail center

--- examples/anscombes_quartet.py
+++ examples/anscombes_quartet.py
@@ -2,6 +2,7 @@
 Anscombe's quartet
 ==================
 
+_thumb: .4, .4
 """
 import seaborn as sns
 sns.set(style="ticks")
--- examples/marginal_ticks.py
+++ examples/marginal_ticks.py
@@ -2,6 +2,7 @@
 Scatterplot with marginal ticks
 ===============================
 
+_thumb: .65, .35
 """
 import numpy as np
 import seaborn as sns
@@ -14,6 +15,6 @@ cov = [(1, 0), (0, 2)]
 x, y = rs.multivariate_normal(mean, cov, 100).T
 
 color = sns.color_palette()[1]
-grid = sns.JointGrid(x, y, space=0, size=7, ratio=50)
+grid = sns.JointGrid(x, y, space=0, size=6, ratio=50)
 grid.plot_joint(plt.scatter, color=color, alpha=.8)
 grid.plot_marginals(sns.rugplot, color=color)
