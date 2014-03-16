a2958e4fb74842d3b0515c529f9a736fe86df246
mwaskom <mwaskom@stanford.edu>
2014-03-16 09:34:06 -0700

    More examples, starting with a grid of 24

---
+++ examples/faceted_histogram.py
@@ -0,0 +1,15 @@
+"""
+Facetting histograms by subsets of data
+=======================================
+
+_thumb: .42, .57
+"""
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set(style="darkgrid")
+
+tips = sns.load_dataset("tips")
+g = sns.FacetGrid(tips, row="sex", col="time", margin_titles=True)
+bins = np.linspace(0, 60, 13)
+g.map(plt.hist, "total_bill", color="steelblue", bins=bins, lw=0)
