27cc2c0f11d20808c825041368d2e84dac2e1523
mwaskom <mwaskom@stanford.edu>
2014-03-21 15:40:42 -0700

    Update docs with new style information

---
+++ doc/tools/nb_to_doc.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+"""
+Convert empty IPython notebook to a sphinx doc page.
+
+"""
+import os
+import sys
+
+
+def convert_nb(nbname):
+
+    os.system("runipy --o %s.ipynb --matplotlib --quiet" % nbname)
+    os.system("ipython nbconvert --to rst %s.ipynb" % nbname)
+    os.system("tools/nbstripout %s.ipynb" % nbname)
+
+
+if __name__ == "__main__":
+
+    for nbname in sys.argv[1:]:
+        convert_nb(nbname.strip(".ipynb"))
