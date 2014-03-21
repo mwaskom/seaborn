04e2af6f322715efdcb83501dbec8ffae6171d86
mwaskom <mwaskom@stanford.edu>
2014-03-21 15:39:53 -0700

    Update example scripts

--- examples/violinplots.py
+++ examples/violinplots.py
@@ -5,8 +5,9 @@ Violinplots
 """
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-sns.set(style="whitegrid")
+sns.set(style="ticks")
 
 rs = np.random.RandomState(0)
 
@@ -14,5 +15,7 @@ n, p = 40, 8
 d = rs.normal(0, 1, (n, p))
 d += np.log(np.arange(1, p + 1)) * -5 + 10
 
+f, ax = plt.subplots()
+sns.offset_spines()
 sns.violinplot(d)
-sns.despine(left=True)
+sns.despine(left=True, trim=True)
