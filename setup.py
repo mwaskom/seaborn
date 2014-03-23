7ab92cc4b1eb3fd849321af93c4f9b91c71f87e7
mwaskom <mwaskom@stanford.edu>
2014-03-22 17:19:39 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -3,32 +3,17 @@
 # Copyright (C) 2012-2014 Michael Waskom <mwaskom@stanford.edu>
 
 DESCRIPTION = "Seaborn: statistical data visualization"
-LONG_DESCRIPTION = """\
-Seaborn is a library for making attractive and informative statistical graphics in Python. It is built on top of matplotlib and tightly integrated with the PyData stack, including support for numpy and pandas data structures and statistical routines from scipy and statsmodels.
-
-Some of the features that seaborn offers are
-
-- Several built-in themes that improve on the default matplotlib aesthetics
-- Tools for choosing color palettes to make beautiful plots that reveal patterns in your data
-- Functions for visualizing univariate and bivariate distributions or for comparing them between subsets of data
-- Tools that fit and visualize linear regression models for different kinds of independent and dependent variables
-- A function to plot statistical timeseries data with flexible estimation and representation of uncertainty around the estimate
-- High-level abstractions for structuring grids of plots that let you easily build complex visualizations
-"""
-
 DISTNAME = 'seaborn'
 MAINTAINER = 'Michael Waskom'
 MAINTAINER_EMAIL = 'mwaskom@stanford.edu'
 URL = 'http://stanford.edu/~mwaskom/software/seaborn/'
 LICENSE = 'BSD (3-clause)'
-DOWNLOAD_URL = ('https://github.com/mwaskom/seaborn/zipball/master'
-                '#egg=seaborn=dev')
-VERSION = '0.3.dev'
+DOWNLOAD_URL = 'https://github.com/mwaskom/seaborn/'
+VERSION = '0.3'
 
 from setuptools import setup
 
 if __name__ == "__main__":
-
     import os
     if os.path.exists('MANIFEST'):
         os.remove('MANIFEST')
