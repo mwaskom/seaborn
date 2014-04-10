298a43da28c803e7872eead9b99d1a8a216d8c20
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-04-10 16:13:48 -0700

    one test fully works! :)

--- seaborn/tests/test_clustering.py
+++ seaborn/tests/test_clustering.py
@@ -6,6 +6,7 @@ import pandas as pd
 import nose.tools as nt
 import numpy.testing as npt
 import pandas.util.testing as pdt
+import scipy
 from numpy.testing.decorators import skipif
 
 from .. import clustering as cl
@@ -55,39 +56,69 @@ class TestClusteredHeatmapPlotter(object):
                        'label_loc': 'dendrogram', 'label': True,
                        'cluster': True, 'fontsize': None}
 
+    default_pcolormesh_kws = {'linewidth': 0, 'edgecolor': 'white'}
+    default_colorbar_kws = {'fontsize': None, 'label': 'values'}
+
     def test_interpret_kws_from_none_divergent(self):
         p = cl._ClusteredHeatmapPlotter(self.data2d)
         p.interpret_kws(row_kws=None, col_kws=None, pcolormesh_kws=None,
-                        colorbar_kws=None)
-
-
+                        dendrogram_kws=None, colorbar_kws=None)
         pdt.assert_dict_equal(p.row_kws, self.default_dim_kws)
         pdt.assert_dict_equal(p.col_kws, self.default_dim_kws)
 
-        nt.assert_is_instance(p.cmap, mpl.cm.BuRd_r)
+        nt.assert_equal(p.cmap, mpl.cm.RdBu_r)
         pdt.assert_dict_equal(p.pcolormesh_kws, self.default_pcolormesh_kws)
         pdt.assert_dict_equal(p.colorbar_kws, self.default_colorbar_kws)
-        nt.assert_is_none(p.norm)
+
+        abs_max = np.abs(self.data2d.max().max())
+        abs_min = np.abs(self.data2d.min().min())
+        vmaxx = max(abs_max, abs_min)
+        nt.assert_almost_equal(p.vmin, -vmaxx)
+        nt.assert_almost_equal(p.vmax, vmaxx)
 
     def test_interpret_kws_from_none_positive(self):
         p = cl._ClusteredHeatmapPlotter(np.abs(self.data2d))
         p.interpret_kws(row_kws=None, col_kws=None, pcolormesh_kws=None,
-                        colorbar_kws=None)
-        nt.assert_is_instance(p.cmap, mpl.cm.YlGnBu)
+                        dendrogram_kws=None, colorbar_kws=None)
+        nt.assert_equal(p.cmap, mpl.cm.YlGnBu)
         nt.assert_is_none(p.norm)
 
     def test_interpret_kws_from_none_log(self):
-        p = cl._ClusteredHeatmapPlotter(np.abs(self.data2d))
+        p = cl._ClusteredHeatmapPlotter(np.log(self.data2d), color_scale='log')
         p.interpret_kws(row_kws=None, col_kws=None, pcolormesh_kws=None,
-                        colorbar_kws=None, color_scale='log')
-        nt.assert_is_instance(p.norm, mpl.colors.LogNorm)
-        nt.assert_is_instance(p.cmap, mpl.cm.YlGnBu)
-        pass
+                        dendrogram_kws=None, colorbar_kws=None)
+        nt.assert_equal(p.norm, mpl.colors.LogNorm)
+        nt.assert_equal(p.cmap, mpl.cm.YlGnBu)
 
-    def test_calculate_linkage(self):
-        pass
+    def test_calculate_linkage_linear(self):
+        import scipy.cluster.hierarchy as sch
+
+        scipy.random.seed(2013)
+        row_linkage = sch.linkage(self.data2d.values, metric='euclidean',
+                              method='average')
+        col_linkage = sch.linkage(self.data2d.values.T, metric='euclidean',
+                                  method='average')
+        p = cl._ClusteredHeatmapPlotter(self.data2d)
+
+        scipy.random.seed(2013)
+        p.calculate_linkage()
+        npt.assert_array_equal(p.row_linkage, row_linkage)
+        npt.assert_array_equal(p.col_linkage, col_linkage)
+
+    def test_calculate_linkage_log(self):
+        import scipy.cluster.hierarchy as sch
+        values = np.log10(self.data2d.values)
+        row_linkage = sch.linkage(values, metric='euclidean',
+                              method='average')
+        col_linkage = sch.linkage(values.T, metric='euclidean',
+                                  method='average')
+        p = cl._ClusteredHeatmapPlotter(self.data2d, color_scale='log')
+        p.calculate_linkage()
+        npt.assert_array_equal(p.row_linkage, row_linkage)
+        npt.assert_array_equal(p.col_linkage, col_linkage)
 
     def test_get_width_ratios(self):
+
         pass
 
     def test_color_list_to_matrix_and_cmap(self):
@@ -104,4 +135,26 @@ class TestClusteredHeatmapPlotter(object):
 
     def test_label_dimension(self):
         pass
+
+    def test_plot_heatmap(self):
+        pass
+
+    def test_set_title(self):
+        pass
+
+    def test_colorbar(self):
+        pass
+
+    def test_plot_col_side(self):
+        pass
+
+    def test_plot_row_side(self):
+        pass
+
+    def test_label(self):
+        pass
+
+    def test_plot(self):
+        pass
+
 
