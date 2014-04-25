caaf3b9f52c1f3005b7c752a72d937f05dec902c
mwaskom <mwaskom@stanford.edu>
2014-04-25 09:41:08 -0700

    Add cubehelix_palette function

---
+++ examples/cubehelix_palette.py
@@ -0,0 +1,16 @@
+"""
+Different cubehelix palettes
+============================
+
+"""
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.set(style="dark")
+
+f, axes = plt.subplots(9, figsize=(8, 8))
+for ax, s in zip(axes, np.linspace(0, 3, 10)):
+    pal = sns.cubehelix_palette(10, s)
+    ax.imshow([pal], interpolation="nearest")
+    ax.set(yticks=[], xticklabels=[])
