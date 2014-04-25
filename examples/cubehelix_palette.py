38e3263fa1df60af923607a927c4057ea87364ef
mwaskom <mwaskom@stanford.edu>
2014-04-25 10:41:31 -0700

    Add hexbin_marginals example and tweak cubehelix example

--- examples/cubehelix_palette.py
+++ examples/cubehelix_palette.py
@@ -2,6 +2,7 @@
 Different cubehelix palettes
 ============================
 
+_thumb: .4, .65
 """
 import numpy as np
 import seaborn as sns
@@ -14,3 +15,4 @@ for ax, s in zip(axes, np.linspace(0, 3, 10)):
     pal = sns.cubehelix_palette(10, s)
     ax.imshow([pal], interpolation="nearest")
     ax.set(yticks=[], xticklabels=[])
+f.tight_layout()
