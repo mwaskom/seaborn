c86a06e5222710fc247b480af81a33098666050a
mwaskom <mwaskom@stanford.edu>
2014-04-26 10:01:15 -0700

    Update docs and examples with new spine offset functionality

--- examples/grouped_boxplot.py
+++ examples/grouped_boxplot.py
@@ -4,12 +4,12 @@ Grouped boxplots
 
 """
 import seaborn as sns
-sns.set(style="whitegrid")
+sns.set(style="ticks")
 
 tips = sns.load_dataset("tips")
 days = ["Thur", "Fri", "Sat", "Sun"]
 
 g = sns.factorplot("day", "total_bill", "sex", tips, kind="box",
                    palette="PRGn", aspect=1.25, x_order=days)
-g.despine(left=True)
+g.despine(offset=10, trim=True)
 g.set_axis_labels("Day", "Total Bill")
