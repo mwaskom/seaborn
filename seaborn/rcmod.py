ff69c08c5edfcd958c88b0ff09b7ff9da65ab1d6
Erik Ziegler <erik.ziegler@ulg.ac.be>
2014-05-15 11:36:01 +0200

    Added pdf.fonttype=42 to rcmod for Illustration-editable text

--- seaborn/rcmod.py
+++ seaborn/rcmod.py
@@ -207,6 +207,7 @@ def axes_style(style=None, rc=None):
             "font.family": "Arial",
             "grid.linestyle": "-",
             "lines.solid_capstyle": "round",
+            "pdf.fonttype": 42,
             }
 
         # Set grid on or off
