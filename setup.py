0a896f69c961b122df2d0b62494addc59acd2519
Olga Botvinnik <olga.botvinnik@gmail.com>
2014-07-15 18:35:39 -0700

    specify this version as "clustering"

--- setup.py
+++ setup.py
@@ -24,6 +24,12 @@ LICENSE = 'BSD (3-clause)'
 DOWNLOAD_URL = 'https://github.com/mwaskom/seaborn/'
 VERSION = '0.4.clustering'
 
+try:
+    from setuptools import setup
+    _has_setuptools = True
+except ImportError:
+    from disutils import setup
+
 
 def check_dependencies():
 
