a52d9954d45bc3895eb64efd3d0a22281732d502
mwaskom <mwaskom@stanford.edu>
2014-11-22 12:52:42 -0800

    Bump version back to dev

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ MAINTAINER_EMAIL = 'mwaskom@stanford.edu'
 URL = 'http://stanford.edu/~mwaskom/software/seaborn/'
 LICENSE = 'BSD (3-clause)'
 DOWNLOAD_URL = 'https://github.com/mwaskom/seaborn/'
-VERSION = '0.5.1'
+VERSION = '0.6.dev'
 
 try:
     from setuptools import setup
