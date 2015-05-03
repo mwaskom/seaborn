4b101793a28f8ff8ef9df5ef76082ede29b03bfb
mwaskom <mwaskom@stanford.edu>
2015-05-03 15:34:13 -0700

    Slightly more efficient approach to restyling boxplots

--- seaborn/categorical.py
+++ seaborn/categorical.py
@@ -310,7 +310,7 @@ class _CategoricalPlotter(object):
 
         def is_categorical(s):
             try:
-                # Correct way, but doesn't exist in older Pandas
+                # Correct way, but doesnt exist in older Pandas
                 return pd.core.common.is_categorical_dtype(s)
             except AttributeError:
                 # Also works, but feels hackier
@@ -478,35 +478,30 @@ class _BoxPlotter(_CategoricalPlotter):
     def restyle_boxplot(self, artist_dict, color, kws):
         """Take a drawn matplotlib boxplot and make it look nice."""
         for box in artist_dict["boxes"]:
-            box.set_color(color)
-            box.set_zorder(.9)
-            box.set_edgecolor(self.gray)
-            box.set_linewidth(self.linewidth)
-            if 'boxprops' in kws:
-                box.update(kws['boxprops'])
+            box.update(dict(color=color,
+                            zorder=.9,
+                            edgecolor=self.gray,
+                            linewidth=self.linewidth))
+            box.update(kws.get("boxprops", {}))
         for whisk in artist_dict["whiskers"]:
-            whisk.set_color(self.gray)
-            whisk.set_linewidth(self.linewidth)
-            whisk.set_linestyle("-")
-            if 'whiskerprops' in kws:
-                whisk.update(kws['whiskerprops'])
+            whisk.update(dict(color=self.gray,
+                              linewidth=self.linewidth,
+                              linestyle="-"))
+            whisk.update(kws.get("whiskerprops", {}))
         for cap in artist_dict["caps"]:
-            cap.set_color(self.gray)
-            cap.set_linewidth(self.linewidth)
-            if 'capprops' in kws:
-                cap.update(kws['capprops'])
+            cap.update(dict(color=self.gray,
+                            linewidth=self.linewidth))
+            cap.update(kws.get("capprops", {}))
         for med in artist_dict["medians"]:
-            med.set_color(self.gray)
-            med.set_linewidth(self.linewidth)
-            if 'medianprops' in kws:
-                med.update(kws['medianprops'])
+            med.update(dict(color=self.gray,
+                            linewidth=self.linewidth))
+            med.update(kws.get("medianprops", {}))
         for fly in artist_dict["fliers"]:
-            fly.set_color(self.gray)
-            fly.set_marker("d")
-            fly.set_markeredgecolor(self.gray)
-            fly.set_markersize(self.fliersize)
-            if 'flierprops' in kws:
-                fly.update(kws['flierprops'])
+            fly.update(dict(color=self.gray,
+                            marker="d",
+                            markeredgecolor=self.gray,
+                            markersize=self.fliersize))
+            fly.update(kws.get("flierprops", {}))
 
     def plot(self, ax, boxplot_kws):
         """Make the plot."""
