43c9f79ec5471ea3988851a15d56ff6c3844d65a
Marcel Martin <marcel.martin@scilifelab.se>
2015-07-09 10:32:57 +0200

    Remove all remaining uses of distance.squareform.
    The tests in which they occurred did not fail for the following reasons:
    * In test_custom_linkage, _DendrogramPlotter just uses the passed linkage
      and does not compute its own.
    * In TestClustermap, the linkage itself is never used directly.

--- seaborn/tests/test_matrix.py
+++ seaborn/tests/test_matrix.py
@@ -467,8 +467,7 @@ class TestDendrogram(object):
             linkage = fastcluster.linkage_vector(self.x_norm, method='single',
                                                  metric='euclidean')
         except ImportError:
-            d = distance.squareform(distance.pdist(self.x_norm,
-                                                   metric='euclidean'))
+            d = distance.pdist(self.x_norm, metric='euclidean')
             linkage = hierarchy.linkage(d, method='single')
         dendrogram = hierarchy.dendrogram(linkage, no_plot=True,
                                           color_list=['k'],
@@ -597,8 +596,7 @@ class TestClustermap(object):
                                                     metric='euclidean',
                                                     method='single')
     except ImportError:
-        x_norm_distances = distance.squareform(
-            distance.pdist(x_norm.T, metric='euclidean'))
+        x_norm_distances = distance.pdist(x_norm.T, metric='euclidean')
         x_norm_linkage = hierarchy.linkage(x_norm_distances, method='single')
     x_norm_dendrogram = hierarchy.dendrogram(x_norm_linkage, no_plot=True,
                                              color_list=['k'],
