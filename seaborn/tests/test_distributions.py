27e1a9efc1b6dc65db6356aed591c514bba1dc30
Rob Story <rob@simple.com>
2015-08-09 22:12:59 -0700

    REF: Call np.random.normal for only 10 points so we don't slow down tests

--- seaborn/tests/test_distributions.py
+++ seaborn/tests/test_distributions.py
@@ -239,7 +239,7 @@ class TestJointPlot(object):
 
 class TestDistPlot(object):
 
-    data = np.random.normal(0, 0.1, 1000)
+    data = np.random.normal(0, 0.1, 10)
 
     def test_distplot_legend_render(self):
 
