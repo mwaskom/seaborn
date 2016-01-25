551db95d5dc456f78edc37dca7900102a0879d33
mwaskom <mwaskom@stanford.edu>
2016-01-24 19:15:30 -0800

    Update DataFrame sort in example scripts

--- examples/horizontal_barplot.py
+++ examples/horizontal_barplot.py
@@ -11,7 +11,7 @@ sns.set(style="whitegrid")
 f, ax = plt.subplots(figsize=(6, 15))
 
 # Load the example car crash dataset
-crashes = sns.load_dataset("car_crashes").sort("total", ascending=False)
+crashes = sns.load_dataset("car_crashes").sort_values("total", ascending=False)
 
 # Plot the total crashes
 sns.set_color_codes("pastel")
