cdc214791ce390a40bcbd0abc8b5550001e5cb73
Gravish, Nick <nickgravish@Nicks-iMac.local>
2016-02-18 08:13:38 -0500

    small change to example, added off_diag plots

--- examples/custom_map_diag_hist.py
+++ examples/custom_map_diag_hist.py
@@ -12,25 +12,27 @@ sns.set(style="white", palette="muted", color_codes=True)
 rs = np.random.RandomState(10)
 
 # make some test data with a boolean label of unequal sample size
-df = pd.DataFrame({'data1':np.random.randn(20000),
-                   'data2':np.random.randn(20000),
-                   'label': np.random.randn(20000) > 0})
+df = pd.DataFrame({'data1':np.random.randn(2000),
+                   'data2':np.random.randn(2000),
+                   'label': np.random.randn(2000) > 0})
 
 # Default barstacked histogram
-g = sns.PairGrid(df, x_vars= ['data1', 'data2'],
+g1 = sns.PairGrid(df, x_vars= ['data1', 'data2'],
                  y_vars= ['data1', 'data2'],
                  hue = "label", diag_sharey = False,
                  palette = "Set1")
 
-g.map_diag(plt.hist, normed = False, bins = np.arange(-5,5,.25))
+g1.map_offdiag(plt.scatter)
+g1.map_diag(plt.hist, normed = False, bins = np.arange(-5,5,.25))
 
 # Updated custom histtype, and also illustrate how normalization should appear
-g = sns.PairGrid(df, x_vars= ['data1', 'data2'],
+g2 = sns.PairGrid(df, x_vars= ['data1', 'data2'],
                  y_vars= ['data1', 'data2'],
                  hue = "label", diag_sharey = False,
                  palette = "Set1")
 
-g.map_diag(plt.hist, normed = True,
+g2.map_offdiag(plt.scatter)
+g2.map_diag(plt.hist, normed = True,
                      alpha = 0.8,
                      histtype = 'step',
                      linewidth = 3,
