9be4d59a7cd08885bf915e83d4ffb947b512d779
mwaskom <mwaskom@stanford.edu>
2017-02-04 16:35:15 -0500

    Update copyright year

--- doc/conf.py
+++ doc/conf.py
@@ -65,7 +65,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'seaborn'
-copyright = u'2012-2016, Michael Waskom'
+copyright = u'2012-2017, Michael Waskom'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
