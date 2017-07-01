228cc572f6595bc65264fc1ca79513a377d0bffa
mwaskom <mwaskom@stanford.edu>
2017-07-01 14:49:37 -0400

    Deprecate seaborn.apionly

--- seaborn/apionly.py
+++ seaborn/apionly.py
@@ -1,2 +1,9 @@
+import warnings
+msg = (
+"The seaborn.apionly module is deprecated, as seaborn no longer sets a default "
+"style on import. It will be removed in a future version."
+)
+warnings.warn(msg, UserWarning)
+
 from seaborn import *
 reset_orig()
