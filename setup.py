1f562a1a8d45f99c4ec607269f26e202618f8e43
mwaskom <mwaskom@stanford.edu>
2017-07-08 19:34:09 -0400

    Fix python version tags in setup file

--- setup.py
+++ setup.py
@@ -81,8 +81,9 @@ if __name__ == "__main__":
         classifiers=[
                      'Intended Audience :: Science/Research',
                      'Programming Language :: Python :: 2.7',
-                     'Programming Language :: Python :: 3.3',
                      'Programming Language :: Python :: 3.4',
+                     'Programming Language :: Python :: 3.5',
+                     'Programming Language :: Python :: 3.6',
                      'License :: OSI Approved :: BSD License',
                      'Topic :: Scientific/Engineering :: Visualization',
                      'Topic :: Multimedia :: Graphics',
