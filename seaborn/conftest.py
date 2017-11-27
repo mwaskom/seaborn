fa507e16a683e3ce86f6ac17f1537dfffc966e15
Michael Waskom <mwaskom@nyu.edu>
2017-11-27 09:06:10 -0500

    Attempt to workarond pandas 0.21 datetime converter issue for doctests

--- seaborn/conftest.py
+++ seaborn/conftest.py
@@ -2,6 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pytest
 
+# Attempted workaround for change in pandas 0.21
+# Can be removed when matplotlib 2.2 is released
+from pandas.tseries import converter
+converter.register()
+
 
 @pytest.fixture(autouse=True)
 def close_figs():
