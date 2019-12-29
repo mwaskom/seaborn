4ed81328aa8f9c3d5033684573c57a9b9bc6d530
Michael Waskom <mwaskom@nyu.edu>
2019-12-29 15:56:38 -0500

    Re-pin minor versions

--- setup.py
+++ setup.py
@@ -29,10 +29,10 @@ DOWNLOAD_URL = 'https://github.com/mwaskom/seaborn/'
 VERSION = '0.9.1.dev'
 
 INSTALL_REQUIRES = [
-    'numpy>=1.10',
-    'scipy>=0.17',
-    'pandas>=0.17',
-    'matplotlib>=1.5',
+    'numpy>=1.10.4',
+    'scipy>=0.17.1',
+    'pandas>=0.17.1',
+    'matplotlib>=1.5.3',
 ]
 
 PACKAGES = [
