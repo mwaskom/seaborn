8fe7edc00e62d47b883436749b0a77340608b31d
Michael Waskom <mwaskom@users.noreply.github.com>
2020-01-05 14:01:31 -0500

    Improve color palette robustness (#1912)
    - Address upcoming numpy type-check strictness (fixes #1907)
    - Standardize output of color palette functions as rgb tuples

--- seaborn/palettes.py
+++ seaborn/palettes.py
@@ -311,7 +311,7 @@ def hls_palette(n_colors=6, h=.01, l=.6, s=.65):  # noqa
         >>> sns.palplot(sns.hls_palette(10, s=.4))
 
     """
-    hues = np.linspace(0, 1, n_colors + 1)[:-1]
+    hues = np.linspace(0, 1, int(n_colors) + 1)[:-1]
     hues += h
     hues %= 1
     hues -= hues.astype(int)
@@ -379,7 +379,7 @@ def husl_palette(n_colors=6, h=.01, s=.9, l=.65):  # noqa
         >>> sns.palplot(sns.husl_palette(10, s=.4))
 
     """
-    hues = np.linspace(0, 1, n_colors + 1)[:-1]
+    hues = np.linspace(0, 1, int(n_colors) + 1)[:-1]
     hues += h
     hues %= 1
     hues *= 359
@@ -460,7 +460,7 @@ def mpl_palette(name, n_colors=6):
     if name in MPL_QUAL_PALS:
         bins = np.linspace(0, 1, MPL_QUAL_PALS[name])[:n_colors]
     else:
-        bins = np.linspace(0, 1, n_colors + 2)[1:-1]
+        bins = np.linspace(0, 1, int(n_colors) + 2)[1:-1]
     palette = list(map(tuple, cmap(bins)[:, :3]))
 
     return _ColorPalette(palette)
@@ -684,6 +684,8 @@ def diverging_palette(h_neg, h_pos, s=75, l=50, sep=10, n=6,  # noqa
         Anchor saturation for both extents of the map.
     l : float in [0, 100], optional
         Anchor lightness for both extents of the map.
+    sep : int, optional
+        Size of the intermediate region.
     n : int, optional
         Number of colors in the palette (if not returning a cmap)
     center : {"light", "dark"}, optional
@@ -742,10 +744,10 @@ def diverging_palette(h_neg, h_pos, s=75, l=50, sep=10, n=6,  # noqa
 
     """
     palfunc = dark_palette if center == "dark" else light_palette
-    neg = palfunc((h_neg, s, l), 128 - (sep // 2), reverse=True, input="husl")
-    pos = palfunc((h_pos, s, l), 128 - (sep // 2), input="husl")
-    midpoint = dict(light=[(.95, .95, .95, 1.)],
-                    dark=[(.133, .133, .133, 1.)])[center]
+    n_half = int(128 - (sep // 2))
+    neg = palfunc((h_neg, s, l), n_half, reverse=True, input="husl")
+    pos = palfunc((h_pos, s, l), n_half, input="husl")
+    midpoint = dict(light=[(.95, .95, .95)], dark=[(.133, .133, .133)])[center]
     mid = midpoint * sep
     pal = blend_palette(np.concatenate([neg, mid,  pos]), n, as_cmap=as_cmap)
     return pal
@@ -775,7 +777,8 @@ def blend_palette(colors, n_colors=6, as_cmap=False, input="rgb"):
     name = "blend"
     pal = mpl.colors.LinearSegmentedColormap.from_list(name, colors)
     if not as_cmap:
-        pal = _ColorPalette(pal(np.linspace(0, 1, int(n_colors))))
+        rgb_array = pal(np.linspace(0, 1, int(n_colors)))[:, :3]  # no alpha
+        pal = _ColorPalette(map(tuple, rgb_array))
     return pal
 
 
@@ -973,7 +976,7 @@ def cubehelix_palette(n_colors=6, start=0, rot=.4, gamma=1.0, hue=0.8,
 
     cmap = mpl.colors.LinearSegmentedColormap("cubehelix", cdict)
 
-    x = np.linspace(light, dark, n_colors)
+    x = np.linspace(light, dark, int(n_colors))
     pal = cmap(x)[:, :3].tolist()
     if reverse:
         pal = pal[::-1]
