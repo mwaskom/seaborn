62667d2249dc776dc0078a4280c258364ceb0992
Michael Waskom <mwaskom@users.noreply.github.com>
2020-02-22 12:48:27 -0500

    Use numpy's indexing dtype in bootstrap (#1968)
    Follow-up to #1952 that uses the correct dtype

--- seaborn/algorithms.py
+++ seaborn/algorithms.py
@@ -81,7 +81,7 @@ def bootstrap(*args, **kwargs):
 
     boot_dist = []
     for i in range(int(n_boot)):
-        resampler = integers(0, n, n, dtype=np.int_)
+        resampler = integers(0, n, n, dtype=np.intp)  # intp is indexing dtype
         sample = [a.take(resampler, axis=0) for a in args]
         boot_dist.append(f(*sample, **func_kwargs))
     return np.array(boot_dist)
@@ -96,10 +96,10 @@ def _structured_bootstrap(args, n_boot, units, func, func_kwargs, integers):
 
     boot_dist = []
     for i in range(int(n_boot)):
-        resampler = integers(0, n_units, n_units, dtype=np.int_)
+        resampler = integers(0, n_units, n_units, dtype=np.intp)
         sample = [np.take(a, resampler, axis=0) for a in args]
         lengths = map(len, sample[0])
-        resampler = [integers(0, n, n, dtype=np.int_) for n in lengths]
+        resampler = [integers(0, n, n, dtype=np.intp) for n in lengths]
         sample = [[c.take(r, axis=0) for c, r in zip(a, resampler)]
                   for a in sample]
         sample = list(map(np.concatenate, sample))
