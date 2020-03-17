3f7761de1a47eb8b27c2e9c4b2a66979e54856b4
Maoz Gelbart <13831112+MaozGelbart@users.noreply.github.com>
2020-03-17 23:40:43 +0200

    python3-only to_utf8 (#1979)
    Co-authored-by: Michael Waskom <mwaskom@users.noreply.github.com>

--- seaborn/tests/test_utils.py
+++ seaborn/tests/test_utils.py
@@ -5,7 +5,8 @@ import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from cycler import cycler
+from cycle import cycler
+
 import pytest
 import nose
 import nose.tools as nt
@@ -120,12 +121,25 @@ def test_iqr():
     assert_equal(iqr, 2)
 
 
-def test_str_to_utf8():
-    """Test the to_utf8 function: string to Unicode"""
-    s = "\u01ff\u02ff"
+@pytest.mark.parametrize(
+    "s,exp",
+    [
+        ("a", "a"),
+        ("abc", "abc"),
+        (b"a", "a"),
+        (b"abc", "abc"),
+        (bytearray("abc", "utf-8"), "abc"),
+        (bytearray(), ""),
+        (1, "1"),
+        (0, "0"),
+        ([], str([])),
+    ],
+)
+def test_to_utf8(s, exp):
+    """Test the to_utf8 function: object to string"""
     u = utils.to_utf8(s)
-    assert_equal(type(s), type(str()))
-    assert_equal(type(u), type(u"\u01ff\u02ff"))
+    assert_equal(type(u), str)
+    assert_equal(u, exp)
 
 
 class TestSpineUtils(object):
