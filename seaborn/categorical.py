7c2c6fefb817b080917b2d6d23723d3f4f9c52e1
Michael Waskom <mwaskom@nyu.edu>
2020-04-30 17:13:33 -0400

    Add explicit warning in swarmplot about gutters
    Currently set to warn when > 2% of the points overlap; with a little playing
    around, this seemed like where the distribution started to get obscured.
    But it is ad hoc.

--- seaborn/categorical.py
+++ seaborn/categorical.py
@@ -1315,6 +1315,15 @@ class _SwarmPlotter(_CategoricalScatterPlotter):
         off_high = points > high_gutter
         if off_high.any():
             points[off_high] = high_gutter
+
+        gutter_prop = (off_high + off_low).sum() / len(points)
+        if gutter_prop > .02:
+            msg = (
+                "{:.1%} of the points cannot be placed; you may want "
+                "to decrease the size of the markers or use stripplot."
+            ).format(gutter_prop)
+            warnings.warn(msg, UserWarning)
+
         return points
 
     def swarm_points(self, ax, points, center, width, s, **kws):
