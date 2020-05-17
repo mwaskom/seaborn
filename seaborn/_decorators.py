b2f7960ae786b4423d5efdcc29c0ebfa8bf6208f
Michael Waskom <mwaskom@nyu.edu>
2020-05-17 17:44:35 -0400

    Provide more information in the warning

--- seaborn/_decorators.py
+++ seaborn/_decorators.py
@@ -35,8 +35,9 @@ def _deprecate_positional_args(f):
             article = "" if plural else "a "
             warnings.warn(
                 "Pass the following variable{} as {}keyword arg{}: {}. "
-                "From version 0.12, passing positional arguments "
-                "will result in an error or misinterpreted input."
+                "From version 0.12, the only valid positional argument "
+                "will be `data`, and passing other arguments without an "
+                "explcit keyword will result in an error or misinterpretation."
                 .format(plural, article, plural,
                         ", ".join(kwonly_args[:extra_args])),
                 FutureWarning
