d8bcb9446515d2cf339e03554b73286ea4fcc3fe
Michael Waskom <mwaskom@nyu.edu>
2020-06-04 07:42:25 -0400

    Add new infrastructure for generating docs from notebooks

--- doc/tools/nb_to_doc.py
+++ doc/tools/nb_to_doc.py
@@ -1,47 +1,70 @@
 #! /usr/bin/env python
-"""
-Convert empty IPython notebook to a sphinx doc page.
-
-TODO this should be rewritten to use the nbconvert Python API.
-
-"""
 import os
 import sys
-from subprocess import check_call as sh
-
-
-def convert_nb(nbname):
-
-    # Execute the notebook
-    exec_cmdline = [
-        "jupyter", "nbconvert",
-        "--to", "notebook",
-        "--execute",
-        "--inplace",
-        nbname,
-    ]
-
-    kernel = os.environ.get("NB_KERNEL", "")
-    if kernel:
-        exec_cmdline.append(f"--ExecutePreprocessor.kernel_name={kernel}")
-
-    sh(exec_cmdline)
-
-    # Convert to .rst for Sphinx
-    sh(["jupyter", "nbconvert", "--to", "rst", nbname,
-        "--TagRemovePreprocessor.remove_cell_tags={'hide'}",
-        "--TagRemovePreprocessor.remove_input_tags={'hide-input'}",
-        "--TagRemovePreprocessor.remove_all_outputs_tags={'hide-output'}"])
-
-    # Clear notebook output
-    sh(["jupyter", "nbconvert", "--to", "notebook", "--inplace",
-        "--ClearOutputPreprocessor.enabled=True", nbname])
-
-    # Touch the .rst file so it has a later modify time than the source
-    sh(["touch", nbname + ".rst"])
+import nbformat
+from nbconvert import RSTExporter
+from nbconvert.preprocessors import (
+    ExecutePreprocessor,
+    TagRemovePreprocessor,
+    ExtractOutputPreprocessor
+)
+from traitlets.config import Config
 
 
 if __name__ == "__main__":
 
-    for nbname in sys.argv[1:]:
-        convert_nb(nbname)
+    # Get the desired ipynb file path and parse into components
+    _, fpath = sys.argv
+    basedir, fname = os.path.split(fpath)
+    fstem = fname[:-6]
+
+    # Read the notebook
+    with open(fpath) as f:
+        nb = nbformat.read(f, as_version=4)
+
+    # Run the notebook
+    kernel = nb["metadata"]["kernelspec"]["name"]
+    ep = ExecutePreprocessor(
+        timeout=600,
+        kernel_name=kernel,
+        extra_arguments=["--InlineBackend.rc={'figure.dpi': 96}"]
+    )
+    ep.preprocess(nb, {"metadata": {"path": basedir}})
+
+    # Remove the execution result outputs
+    for cell in nb.get("cells", {}):
+        fields = cell.get("outputs", [])
+        for field in fields:
+            if field["output_type"] == "execute_result":
+                fields.remove(field)
+
+    # Convert to .rst formats
+    exp = RSTExporter()
+
+    c = Config()
+    c.TagRemovePreprocessor.remove_cell_tags = {"hide"}
+    c.TagRemovePreprocessor.remove_input_tags = {"hide-input"}
+    c.TagRemovePreprocessor.remove_all_outputs_tags = {"hide-output"}
+    c.ExtractOutputPreprocessor.output_filename_template = \
+        fstem + "/{cell_index}_{index}{extension}"
+
+    exp.register_preprocessor(TagRemovePreprocessor(config=c), True)
+    exp.register_preprocessor(ExtractOutputPreprocessor(config=c), True)
+
+    body, resources = exp.from_notebook_node(nb)
+
+    # Write the .rst file
+    rst_path = os.path.join(basedir, f"{fstem}.rst")
+    with open(rst_path, "w") as f:
+        f.write(body)
+
+    # Write the individual image outputs
+    imdir = os.path.join(basedir, f"{fstem}")
+    if not os.path.exists(imdir):
+        os.mkdir(imdir)
+
+    for imname, imdata in resources["outputs"].items():
+        if imname.startswith(fstem):
+            impath = os.path.join(basedir, f"{imname}")
+            with open(impath, "wb") as f:
+                f.write(imdata)
