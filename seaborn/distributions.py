384891a8b4286739927592202e1c0f8ccd3c152d
Michael Waskom <mwaskom@nyu.edu>
2020-08-21 16:50:46 -0400

    Fix typo

--- seaborn/distributions.py
+++ seaborn/distributions.py
@@ -1449,7 +1449,7 @@ def histplot(
 
 
 histplot.__doc__ = """\
-Plot univeriate or bivariate histograms to show distributions of datasets.
+Plot univariate or bivariate histograms to show distributions of datasets.
 
 A histogram is a classic visualization tool that represents the distribution
 of one or more variables by counting the number of observations that fall within
