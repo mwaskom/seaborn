323bbf011e412d3e916c9c7155bb5b4d8e78e86d
Michael Waskom <mwaskom@nyu.edu>
2020-08-22 12:08:03 -0400

    Update JointGrid-related examples

--- examples/joint_kde.py
+++ examples/joint_kde.py
@@ -4,25 +4,15 @@ Joint kernel density estimate
 
 _thumb: .6, .4
 """
-import numpy as np
-import pandas as pd
 import seaborn as sns
-sns.set(style="white")
+sns.set(style="ticks")
 
-# Generate a random correlated bivariate dataset
-rs = np.random.RandomState(5)
-mean = [0, 0]
-cov = [(1, .5), (.5, 1)]
-x1, x2 = rs.multivariate_normal(mean, cov, 500).T
-x1 = pd.Series(x1, name="$X_1$")
-x2 = pd.Series(x2, name="$X_2$")
+# Load the penguins dataset
+penguins = sns.load_dataset("penguins")
 
 # Show the joint distribution using kernel density estimation
 g = sns.jointplot(
-    x=x1,
-    y=x2,
+    data=penguins,
+    x="culmen_length_mm", y="culmen_depth_mm", hue="species",
     kind="kde",
-    height=7,
-    space=0,
-    thresh=0,
 )
