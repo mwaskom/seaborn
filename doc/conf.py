b35754cb6f0ec5eb3357293fd07e48f2c55acfea
Maoz Gelbart <13831112+MaozGelbart@users.noreply.github.com>
2020-10-27 19:38:31 +0200

    Format single backticks as literal code block

--- doc/conf.py
+++ doc/conf.py
@@ -96,7 +96,7 @@ release = seaborn.__version__
 exclude_patterns = ['_build', 'docstrings']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
-#default_role = None
+default_role = 'literal'
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 #add_function_parentheses = True
