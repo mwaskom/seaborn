786bd08c490591b037186a9eef9858d04e03307b
Michael Waskom <mwaskom@nyu.edu>
2020-12-29 17:21:34 -0500

    Revert change to forced draw command
    The change in #2399 inadvertantly broke seaborn on the macosx mpl
    backend.
    
    See https://github.com/matplotlib/matplotlib/issues/19197
    
    This PR reverts that change until the issue can be sorted out.

--- seaborn/matrix.py
+++ seaborn/matrix.py
@@ -332,7 +332,10 @@ class _HeatMapper:
         ytl = ax.set_yticklabels(yticklabels, rotation="vertical")
 
         # Possibly rotate them if they overlap
-        ax.figure.canvas.draw()
+        # ax.figure.canvas.draw()
+        if hasattr(ax.figure.canvas, "get_renderer"):
+            ax.figure.draw(ax.figure.canvas.get_renderer())
+
         if axis_ticklabels_overlap(xtl):
             plt.setp(xtl, rotation="vertical")
         if axis_ticklabels_overlap(ytl):
@@ -722,7 +725,10 @@ class _DendrogramPlotter(object):
         ytl = ax.set_yticklabels(self.yticklabels, rotation='vertical')
 
         # Force a draw of the plot to avoid matplotlib window error
-        ax.figure.canvas.draw()
+        # ax.figure.canvas.draw()
+        if hasattr(ax.figure.canvas, "get_renderer"):
+            ax.figure.draw(ax.figure.canvas.get_renderer())
+
         if len(ytl) > 0 and axis_ticklabels_overlap(ytl):
             plt.setp(ytl, rotation="horizontal")
         if len(xtl) > 0 and axis_ticklabels_overlap(xtl):
