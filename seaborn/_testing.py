18e7165c1b08f6777c8e956146bc34fa34879207
Michael Waskom <mwaskom@nyu.edu>
2021-01-16 17:04:06 -0500

    Fix plot equality assertion

--- seaborn/_testing.py
+++ seaborn/_testing.py
@@ -3,29 +3,7 @@ import matplotlib as mpl
 from numpy.testing import assert_array_equal
 
 
-LINE_PROPS = [
-    "alpha",
-    "color",
-    "linewidth",
-    "linestyle",
-    "xydata",
-    "zorder",
-]
-
-COLLECTION_PROPS = [
-    "alpha",
-    "edgecolor",
-    "facecolor",
-    "fill",
-    "hatch",
-    "linestyle",
-    "linewidth",
-    "paths",
-    "zorder",
-    "sizes",
-]
-
-BAR_PROPS = [
+USE_PROPS = [
     "alpha",
     "edgecolor",
     "facecolor",
@@ -34,30 +12,36 @@ BAR_PROPS = [
     "height",
     "linestyle",
     "linewidth",
+    "paths",
     "xy",
+    "xydata",
+    "sizes",
     "zorder",
 ]
 
 
-def assert_artists_equal(list1, list2, properties):
+def assert_artists_equal(list1, list2):
 
     assert len(list1) == len(list2)
     for a1, a2 in zip(list1, list2):
+        assert a1.__class__ == a2.__class__
         prop1 = a1.properties()
         prop2 = a2.properties()
-        for key in properties:
+        for key in USE_PROPS:
+            if key not in prop1:
+                continue
             v1 = prop1[key]
             v2 = prop2[key]
             if key == "paths":
                 for p1, p2 in zip(v1, v2):
                     assert_array_equal(p1.vertices, p2.vertices)
                     assert_array_equal(p1.codes, p2.codes)
-            elif isinstance(v1, np.ndarray):
-                assert_array_equal(v1, v2)
             elif key == "color":
                 v1 = mpl.colors.to_rgba(v1)
                 v2 = mpl.colors.to_rgba(v2)
                 assert v1 == v2
+            elif isinstance(v1, np.ndarray):
+                assert_array_equal(v1, v2)
             else:
                 assert v1 == v2
 
@@ -69,18 +53,18 @@ def assert_legends_equal(leg1, leg2):
         assert t1.get_text() == t2.get_text()
 
     assert_artists_equal(
-        leg1.get_patches(), leg2.get_patches(), BAR_PROPS,
+        leg1.get_patches(), leg2.get_patches(),
     )
     assert_artists_equal(
-        leg1.get_lines(), leg2.get_lines(), LINE_PROPS,
+        leg1.get_lines(), leg2.get_lines(),
     )
 
 
 def assert_plots_equal(ax1, ax2, labels=True):
 
-    assert_artists_equal(ax1.patches, ax2.patches, BAR_PROPS)
-    assert_artists_equal(ax1.lines, ax2.lines, LINE_PROPS)
-    assert_artists_equal(ax1.collections, ax2.collections, COLLECTION_PROPS)
+    assert_artists_equal(ax1.patches, ax2.patches)
+    assert_artists_equal(ax1.lines, ax2.lines)
+    assert_artists_equal(ax1.collections, ax2.collections)
 
     if labels:
         assert ax1.get_xlabel() == ax2.get_xlabel()
