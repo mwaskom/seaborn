47031ef5dae45f5e0b8f86b66062d93182b6662f
Michael Waskom <mwaskom@nyu.edu>
2021-01-20 10:53:52 -0500

    Move beeswarm algorithm into distinct class

--- seaborn/categorical.py
+++ seaborn/categorical.py
@@ -1609,16 +1609,12 @@ class _SwarmPlotter(_CategoricalScatterPlotter):
 
         # Update the position of each point on the categorical axis
         # Do this after plotting so that the numerical axis limits are correct
-        orient = self.orient
+        beeswarm = Beeswarm(width=width, orient=self.orient)
         for center, swarm in zip(centers, swarms):
-            swarm._center = center
             if swarm.get_offsets().size:
-
-                # XXX why is center always the final value?
-                def draw(points, renderer, *, center=center, width=width, orient=orient):
-                    swarm_points(points, center, width, orient)
+                def draw(points, renderer, *, center=center):
+                    beeswarm(points, center)
                     super(points.__class__, points).draw(renderer)
-
                 swarm.draw = draw.__get__(swarm)
 
     def plot(self, ax, kws):
@@ -1628,175 +1624,6 @@ class _SwarmPlotter(_CategoricalScatterPlotter):
         self.annotate_axes(ax)
         if self.orient == "h":
             ax.invert_yaxis()
-
-
-def could_overlap(xy_i, swarm, d):
-    """Return a list of all swarm points that could overlap with target.
-
-    Assumes that swarm is a sorted list of all points below xy_i.
-    """
-    _, y_i = xy_i
-    neighbors = []
-    for xy_j in reversed(swarm):
-        _, y_j = xy_j
-        if (y_i - y_j) < d:
-            neighbors.append(xy_j)
-        else:
-            break
-    return np.array(list(reversed(neighbors)))
-
-
-def position_candidates(xy_i, neighbors, d):
-    """Return a list of (x, y) coordinates that might be valid."""
-    candidates = [xy_i]
-    x_i, y_i = xy_i
-    left_first = True
-    for x_j, y_j in neighbors:
-        dy = y_i - y_j
-        dx = np.sqrt(max(d ** 2 - dy ** 2, 0)) * 1.05
-        cl, cr = (x_j - dx, y_i), (x_j + dx, y_i)
-        if left_first:
-            new_candidates = [cl, cr]
-        else:
-            new_candidates = [cr, cl]
-        candidates.extend(new_candidates)
-        left_first = not left_first
-    return np.array(candidates)
-
-
-def first_non_overlapping_candidate(candidates, neighbors, d):
-    """Remove candidates from the list if they overlap with the swarm."""
-
-    # IF we have no neighbours, all candidates are good.
-    if len(neighbors) == 0:
-        return candidates[0]
-
-    neighbors_x = neighbors[:, 0]
-    neighbors_y = neighbors[:, 1]
-
-    d_square = d ** 2
-
-    for xy_i in candidates:
-        x_i, y_i = xy_i
-
-        dx = neighbors_x - x_i
-        dy = neighbors_y - y_i
-
-        sq_distances = np.power(dx, 2.0) + np.power(dy, 2.0)
-
-        # good candidate does not overlap any of neighbors
-        # which means that squared distance between candidate
-        # and any of the neighbours has to be at least
-        # square of the diameter
-        good_candidate = np.all(sq_distances >= d_square)
-
-        if good_candidate:
-            return xy_i
-
-    # If `position_candidates` works well
-    # this should never happen
-    raise Exception('No non-overlapping candidates found. '
-                    'This should not happen.')
-
-
-def beeswarm(orig_xy, d):
-    """Adjust x position of points to avoid overlaps."""
-    # In this method, ``x`` is always the categorical axis
-    # Center of the swarm, in point coordinates
-    midline = orig_xy[0, 0]
-
-    # Start the swarm with the first point
-    swarm = [orig_xy[0]]
-
-    # Loop over the remaining points
-    for xy_i in orig_xy[1:]:
-
-        # Find the points in the swarm that could possibly
-        # overlap with the point we are currently placing
-        neighbors = could_overlap(xy_i, swarm, d)
-
-        # Find positions that would be valid individually
-        # with respect to each of the swarm neighbors
-        candidates = position_candidates(xy_i, neighbors, d)
-
-        # Sort candidates by their centrality
-        offsets = np.abs(candidates[:, 0] - midline)
-        candidates = candidates[np.argsort(offsets)]
-
-        # Find the first candidate that does not overlap any neighbours
-        new_xy_i = first_non_overlapping_candidate(candidates, neighbors, d)
-
-        # Place it into the swarm
-        swarm.append(new_xy_i)
-
-    return np.array(swarm)
-
-
-def add_gutters(points, center, width):
-    """Stop points from extending beyond their territory."""
-    half_width = width / 2
-    low_gutter = center - half_width
-    off_low = points < low_gutter
-    if off_low.any():
-        points[off_low] = low_gutter
-    high_gutter = center + half_width
-    off_high = points > high_gutter
-    if off_high.any():
-        points[off_high] = high_gutter
-
-    gutter_prop = (off_high + off_low).sum() / len(points)
-    # XXX this will fire multiple times now, which we don't want
-    if gutter_prop > .05:
-        msg = (
-            "{:.1%} of the points cannot be placed; you may want "
-            "to decrease the size of the markers or use stripplot."
-        ).format(gutter_prop)
-        warnings.warn(msg, UserWarning)
-
-    return points
-
-
-def swarm_points(points, center, width, orient):
-
-    # Convert from point size (area) to diameter
-    ax = points.axes
-    dpi = ax.figure.dpi
-
-    # XXX TODO adapt so s (and lw?) can be vectors
-    s = points.get_sizes().item()
-    lw = points.get_linewidth().item()
-    d = (np.sqrt(s) + lw) * (dpi / 72)
-
-    orig_xy_data = points.get_offsets()
-
-    # Order the variables so that x is the categorical axis
-    if orient == "h":
-        orig_xy_data = orig_xy_data[:, [1, 0]]
-
-    # center = points._center  # XXX hack?
-    orig_xy_data[:, 0] = center
-
-    # Transform the data coordinates to point coordinates.
-    # We'll figure out the swarm positions in the latter
-    # and then convert back to data coordinates and replot
-    orig_xy = ax.transData.transform(orig_xy_data)
-
-    # Do the beeswarm in point coordinates
-    new_xy = beeswarm(orig_xy, d)
-
-    # Transform the point coordinates back to data coordinates
-    if orient == "h":
-        new_xy = new_xy[:, [1, 0]]
-    new_x, new_y = ax.transData.inverted().transform(new_xy).T
-
-    # Add gutters
-    if orient == "v":
-        add_gutters(new_x, center, width)
-    else:
-        add_gutters(new_y, center, width)
-
-    # Reposition the points so they do not overlap
-    points.set_offsets(np.c_[new_x, new_y])
 
 
 class _CategoricalStatPlotter(_CategoricalPlotter):
@@ -4436,3 +4263,176 @@ catplot.__doc__ = dedent("""\
         <seaborn.axisgrid.FacetGrid object at 0x...>
 
     """).format(**_categorical_docs)
+
+
+class Beeswarm:
+
+    def __init__(self, orient, width, warn_gutter_prop=.05):
+
+        self.orient = orient
+        self.width = width
+        self.warn_gutter_prop = warn_gutter_prop
+
+    def __call__(self, points, center):
+
+        # Convert from point size (area) to diameter
+        ax = points.axes
+        dpi = ax.figure.dpi
+
+        # XXX TODO adapt so s (and lw?) can be vectors
+        s = points.get_sizes().item()
+        lw = points.get_linewidth().item()
+        d = (np.sqrt(s) + lw) * (dpi / 72)
+
+        # Get the original positions of the points
+        orig_xy_data = points.get_offsets()
+
+        # Reset the categorical positions to the center line
+        cat_idx = 1 if self.orient == "h" else 0
+        orig_xy_data[:, cat_idx] = center
+
+        # Transform the data coordinates to point coordinates.
+        # We'll figure out the swarm positions in the latter
+        # and then convert back to data coordinates and replot
+        orig_xy = ax.transData.transform(orig_xy_data)
+
+        # Order the variables so that x is the categorical axis
+        if self.orient == "h":
+            orig_xy = orig_xy[:, [1, 0]]
+
+        # Do the beeswarm in point coordinates
+        new_xy = self.beeswarm(orig_xy, d)
+
+        # Transform the point coordinates back to data coordinates
+        if self.orient == "h":
+            new_xy = new_xy[:, [1, 0]]
+        new_x, new_y = ax.transData.inverted().transform(new_xy).T
+
+        # Add gutters
+        if self.orient == "v":
+            self.add_gutters(new_x, center)
+        else:
+            self.add_gutters(new_y, center)
+
+        # Reposition the points so they do not overlap
+        points.set_offsets(np.c_[new_x, new_y])
+
+    def beeswarm(self, orig_xy, d):
+        """Adjust x position of points to avoid overlaps."""
+        # In this method, `x` is always the categorical axis
+        # Center of the swarm, in point coordinates
+        midline = orig_xy[0, 0]
+
+        # Start the swarm with the first point
+        swarm = [orig_xy[0]]
+
+        # Loop over the remaining points
+        for xy_i in orig_xy[1:]:
+
+            # Find the points in the swarm that could possibly
+            # overlap with the point we are currently placing
+            neighbors = self.could_overlap(xy_i, swarm, d)
+
+            # Find positions that would be valid individually
+            # with respect to each of the swarm neighbors
+            candidates = self.position_candidates(xy_i, neighbors, d)
+
+            # Sort candidates by their centrality
+            offsets = np.abs(candidates[:, 0] - midline)
+            candidates = candidates[np.argsort(offsets)]
+
+            # Find the first candidate that does not overlap any neighbors
+            new_xy_i = self.first_non_overlapping_candidate(candidates, neighbors, d)
+
+            # Place it into the swarm
+            swarm.append(new_xy_i)
+
+        return np.array(swarm)
+
+    def could_overlap(self, xy_i, swarm, d):
+        """Return a list of all swarm points that could overlap with target.
+
+        Assumes that swarm is a sorted list of all points below xy_i.
+        """
+        _, y_i = xy_i
+        neighbors = []
+        for xy_j in reversed(swarm):
+            _, y_j = xy_j
+            if (y_i - y_j) < d:
+                neighbors.append(xy_j)
+            else:
+                break
+        return np.array(list(reversed(neighbors)))
+
+    def position_candidates(self, xy_i, neighbors, d):
+        """Return a list of (x, y) coordinates that might be valid."""
+        candidates = [xy_i]
+        x_i, y_i = xy_i
+        left_first = True
+        for x_j, y_j in neighbors:
+            dy = y_i - y_j
+            dx = np.sqrt(max(d ** 2 - dy ** 2, 0)) * 1.05
+            cl, cr = (x_j - dx, y_i), (x_j + dx, y_i)
+            if left_first:
+                new_candidates = [cl, cr]
+            else:
+                new_candidates = [cr, cl]
+            candidates.extend(new_candidates)
+            left_first = not left_first
+        return np.array(candidates)
+
+    def first_non_overlapping_candidate(self, candidates, neighbors, d):
+        """Remove candidates from the list if they overlap with the swarm."""
+
+        # IF we have no neighbors, all candidates are good.
+        if len(neighbors) == 0:
+            return candidates[0]
+
+        neighbors_x = neighbors[:, 0]
+        neighbors_y = neighbors[:, 1]
+
+        d_square = d ** 2
+
+        for xy_i in candidates:
+            x_i, y_i = xy_i
+
+            dx = neighbors_x - x_i
+            dy = neighbors_y - y_i
+
+            sq_distances = np.power(dx, 2.0) + np.power(dy, 2.0)
+
+            # Good candidate does not overlap any of neighbors
+            # which means that squared distance between candidate
+            # and any of the neighbors has to be at least
+            # square of the diameter
+            good_candidate = np.all(sq_distances >= d_square)
+
+            if good_candidate:
+                return xy_i
+
+        # If `position_candidates` works well
+        # this should never happen
+        raise Exception('No non-overlapping candidates found. '
+                        'This should not happen.')
+
+    def add_gutters(self, points, center):
+        """Stop points from extending beyond their territory."""
+        half_width = self.width / 2
+        low_gutter = center - half_width
+        off_low = points < low_gutter
+        if off_low.any():
+            points[off_low] = low_gutter
+        high_gutter = center + half_width
+        off_high = points > high_gutter
+        if off_high.any():
+            points[off_high] = high_gutter
+
+        gutter_prop = (off_high + off_low).sum() / len(points)
+        if gutter_prop > self.warn_gutter_prop:
+            msg = (
+                "{:.1%} of the points cannot be placed; you may want "
+                "to decrease the size of the markers or use stripplot."
+            ).format(gutter_prop)
+            warnings.warn(msg, UserWarning)
+
+        return points
