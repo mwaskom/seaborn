37769df07c1ea46960e72690e3eec3c8bd1de0ea
Michael Waskom <mwaskom@gmail.com>
2022-06-26 22:33:49 -0400

    Update nextgen docs

--- doc/nextgen/conf.py
+++ doc/nextgen/conf.py
@@ -64,6 +64,11 @@ html_theme_options = {
     "page_sidebar_items": [],
 }
 
+html_context = {
+   # other contexts
+   "default_mode": "light"
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
