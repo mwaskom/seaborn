718a905bd609283cc9563d1392fc0699cb3e7219
Michael Waskom <mwaskom@gmail.com>
2022-06-26 20:30:29 -0400

    Bump version for first beta

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ MAINTAINER_EMAIL = 'mwaskom@gmail.com'
 URL = 'https://seaborn.pydata.org'
 LICENSE = 'BSD (3-clause)'
 DOWNLOAD_URL = 'https://github.com/mwaskom/seaborn/'
-VERSION = '0.12.0a1'
+VERSION = '0.12.0b0'
 PYTHON_REQUIRES = ">=3.7"
 
 INSTALL_REQUIRES = [
