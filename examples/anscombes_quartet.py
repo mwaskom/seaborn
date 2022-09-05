d6f51f0f209ebf40e6da864f7f5424bbf187d463
Michael Waskom <mwaskom@gmail.com>
2022-09-05 12:52:51 -0400

    Update errorbar API in example gallery and add new strip/reg example

--- examples/anscombes_quartet.py
+++ examples/anscombes_quartet.py
@@ -11,6 +11,8 @@ sns.set_theme(style="ticks")
 df = sns.load_dataset("anscombe")
 
 # Show the results of a linear regression within each dataset
-sns.lmplot(x="x", y="y", col="dataset", hue="dataset", data=df,
-           col_wrap=2, ci=None, palette="muted", height=4,
-           scatter_kws={"s": 50, "alpha": 1})
+sns.lmplot(
+    data=df, x="x", y="y", col="dataset", hue="dataset",
+    col_wrap=2, palette="muted", ci=None,
+    height=4, scatter_kws={"s": 50, "alpha": 1}
+)
