8735494e4cd37ee781500e50554df2f3be2ead41
Ștefan Istrate <stefan.istrate@gmail.com>
2023-02-25 16:47:21 +0000

    Fix heatmap `norm=None` issue (#3276)
    * Fix heatmap  issue.
    
    * Update seaborn/matrix.py
    
    Co-authored-by: Michael Waskom <mwaskom@users.noreply.github.com>
    
    * Fix condition
    
    * Add test for explicit `norm=None`.
    
    ---------
    
    Co-authored-by: Michael Waskom <mwaskom@users.noreply.github.com>

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -265,6 +265,20 @@ class TestHeatmap:
         hm = mat._HeatMapper(self.df_unif, **kws)
         npt.assert_array_equal(cmap(np.inf), hm.cmap(np.inf))
 
+    def test_explicit_none_norm(self):
+
+        vals = np.linspace(.2, 1, 9)
+        cmap = mpl.cm.binary
+        _, (ax1, ax2) = plt.subplots(2)
+
+        mat.heatmap([vals], vmin=0, cmap=cmap, ax=ax1)
+        fc_default_norm = ax1.collections[0].get_facecolors()
+
+        mat.heatmap([vals], vmin=0, norm=None, cmap=cmap, ax=ax2)
+        fc_explicit_norm = ax2.collections[0].get_facecolors()
+
+        npt.assert_array_almost_equal(fc_default_norm, fc_explicit_norm, 2)
+
     def test_ticklabels_off(self):
         kws = self.default_kws.copy()
         kws['xticklabels'] = False
