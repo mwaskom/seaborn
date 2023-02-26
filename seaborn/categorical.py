53db3596803ee81567299289fafafc14f4d32b73
Michael Waskom <mwaskom@gmail.com>
2023-02-26 15:05:24 -0500

    Define a BoxPlotContainer to hold boxplot artists

--- seaborn/categorical.py
+++ seaborn/categorical.py
@@ -644,7 +644,7 @@ class _CategoricalPlotterNew(_RelationalPlotter):
                             verts[ori_idx][:] = p0, p1
                             line.set_data(verts)
 
-            # TODO Wrap artists in a container and attach to the axes
+            ax.add_container(BoxPlotContainer(artists))
 
     def plot_points(
         self,
@@ -3989,3 +3989,55 @@ class Beeswarm:
             warnings.warn(msg, UserWarning)
 
         return points
+
+
+class BoxPlotContainer:
+
+    def __init__(self, artist_dict):
+
+        self.boxes = artist_dict["boxes"]
+        self.medians = artist_dict["medians"]
+        self.whiskers = artist_dict["whiskers"]
+        self.caps = artist_dict["caps"]
+        self.fliers = artist_dict["fliers"]
+        self.means = artist_dict["means"]
+
+        self._label = None
+        self._children = [
+            *self.boxes,
+            *self.medians,
+            *self.whiskers,
+            *self.caps,
+            *self.fliers,
+            *self.means,
+        ]
+
+    def __repr__(self):
+        return f"<BoxPlotContainer object with {len(self.boxes)} boxes>"
+
+    def __getitem__(self, idx):
+        pair_slice = slice(2 * idx, 2 * idx + 2)
+        return {
+            "box": self.boxes[idx] if self.boxes else [],
+            "median": self.medians[idx] if self.medians else [],
+            "whiskers": self.whiskers[pair_slice] if self.whiskers else [],
+            "caps": self.caps[pair_slice] if self.caps else [],
+            "fliers": self.fliers[idx] if self.fliers else [],
+            "means": self.means[idx]if self.means else [],
+        }
+
+    def __iter__(self):
+        yield from [self[i] for i in range(self.boxes)]
+
+    def get_label(self):
+        return self._label
+
+    def set_label(self, value):
+        self._label = value
+
+    def get_children(self):
+        return self._children
+
+    def remove(self):
+        for child in self._children:
+            child.remove()
